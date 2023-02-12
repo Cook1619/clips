ba4df24348c6911051e3e4ac94e983394cf30457
Cook1619 <cook6348@gmail.com>
2023-02-12 13:56:05 -0600

    Singin form using template forms

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  credentials = {
+    email: '',
+    password: ''
+  }
 
+  signIn(){
+    console.log('Successfully signed in!')
+  }
 }

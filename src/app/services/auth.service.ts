4506996a41289a020967afab016f1ba663bd576a
Matt Cook <mathew.cook@mastery.net>
2023-02-15 09:49:41 -0600

    Setting up signIn/signOut of modal, making sure modal gets destroyed after signing in on a delay

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/compat/auth";
 import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/compat/firestore";
 import IUser from "../../models/user.model";
-import {map, Observable} from "rxjs";
+import {delay, map, Observable} from "rxjs";
 
 
 @Injectable({
@@ -11,9 +11,11 @@ import {map, Observable} from "rxjs";
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>
   public isAuthenticated$: Observable<boolean>
+  public isAuthenticatedWithDelay$: Observable<boolean>
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = db.collection('users')
     this.isAuthenticated$ = auth.user.pipe(map(user => !!user))
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000))
   }
 
   public async createUser(userData: IUser){

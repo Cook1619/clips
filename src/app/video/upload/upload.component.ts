99c9640fe3c273040860f3cfd8616cb64a9526fa
Matt Cook <mathew.cook@mastery.net>
2023-02-16 15:51:59 -0600

    prettier

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -7,17 +7,17 @@ import { Component } from '@angular/core';
 })
 export class UploadComponent {
   isDragOver = false;
-  file: File | null = null
-  nextStep = false
+  file: File | null = null;
+  nextStep = false;
 
   storeFile($event: Event) {
     this.isDragOver = false;
-    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
 
-    if(!this.file || this.file.type !== 'video/mp4'){
-      return
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
     }
-    this.nextStep = true
-    console.log(this.file)
+    this.nextStep = true;
+    console.log(this.file);
   }
 }

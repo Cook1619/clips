c2acb4b1a07531c1c8c438f01d3c0e0e54ac2db4
Matt Cook <mathew.cook@mastery.net>
2023-02-21 11:36:38 -0600

    Sending user to uploading clip page after successful upload

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {
   AngularFirestore,
   AngularFirestoreCollection,
+  DocumentReference,
 } from '@angular/fire/compat/firestore';
 import IClip from '../../models/clip.model';
 
@@ -13,7 +14,7 @@ export class ClipService {
   constructor(private db: AngularFirestore) {
     this.clipsCollection = db.collection('clips');
   }
-  async createClip(data: IClip) {
-    await this.clipsCollection.add(data);
+  createClip(data: IClip): Promise<DocumentReference<IClip>> {
+    return this.clipsCollection.add(data);
   }
 }

f0980300d6d9ee4050d61f830f03c8ce39062848
Matt Cook <mathew.cook@mastery.net>
2023-02-21 13:19:56 -0600

    Creating query to grab clips of the user logged in

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {ClipService} from "../../services/clip.service";
 
 @Component({
   selector: 'app-manage',
@@ -9,16 +10,21 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 export class ManageComponent implements OnInit {
   videoOrder = '1';
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private clipService: ClipService) {
+  }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: Params) => {
       this.videoOrder = params.sort == '2' ? params.sort : '1';
     });
+    this.clipService.getUserClips().subscribe()
   }
 
   sort(event: Event) {
-    const { value } = event.target as HTMLSelectElement;
+    const {value} = event.target as HTMLSelectElement;
 
     // both of these lines of code accomplish the same thing, navigate can do more
     // this.router.navigateByUrl(`/manage?sort=${value}`);

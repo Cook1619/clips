e06a363ca620a46bebc288b38ca97801897b40d5
Matt Cook <mathew.cook@mastery.net>
2023-02-21 11:40:25 -0600

    Adding timestamp to clip model

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -94,6 +94,7 @@ export class UploadComponent implements OnDestroy {
               title: this.title.value,
               fileName: `${clipFileName}.mp4`,
               url,
+              timestamp: firebase.firestore.FieldValue.serverTimestamp()
             };
             const clipDocRef = await this.clipsService.createClip(clip);
             this.alertColor = 'green';
